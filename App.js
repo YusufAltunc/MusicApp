579e04be2a7bc43f3a91337bbb78635220605107
Muhammed Yusuf ALTUNÇ <96544170+YusufAltunc@users.noreply.github.com>
2023-11-10 18:54:05 +0300

    MusicApp
    React native ile müzik uygulaması

---
+++ App.js
@@ -0,0 +1,46 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, FlatList, SafeAreaView } from "react-native";
+import music_data from "./music-data.json";
+import SongCard from "./src/components/SongCard/SongCard";
+import SearchBar from "./src/components/SearchBar";
+import React, { useState } from "react";
+
+export default function App() {
+  const [list, setList] = useState(music_data);
+
+  const renderSong = ({ item }) => <SongCard song={item} />;
+
+  const renderSeperator = () => <View style={styles.seperator} />;
+
+  const handleSearch = (text) => {
+    const filteredList = music_data.filter((song) => {
+      const searcedText = text.toLowerCase();
+      const currentTitle = song.title.toLowerCase();
+
+      return currentTitle.indexOf(searcedText) > -1;
+    });
+    setList(filteredList);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <SearchBar onSearch={handleSearch} />
+      <FlatList
+        keyExtractor={(item) => item.id}
+        data={list}
+        renderItem={renderSong}
+        ItemSeparatorComponent={renderSeperator}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  seperator: {
+    borderWidth: 1,
+    borderColor: "#e0e0e0",
+  },
+});
